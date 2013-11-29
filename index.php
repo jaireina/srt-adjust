a8a1a002c33e500f27223b1d0f4f500948d1a325
Jair Reina <jaireina@gmail.com>
2013-11-29 10:17:45 -0600

    added some comments

--- index.php
+++ index.php
@@ -3,21 +3,25 @@
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
 
-  $offset = "2";
-  $offset_type = -1;
+  //options
+  $offset = "2";        // seconds to bring forward or back
+  $offset_type = -1;    // 1 for forward -1 for back
+  $file = "test.srt";   // name of the srt file to edit
+  $new_file = "new.srt";// new file name
 
-  $contents = explode("\n", file_get_contents("test.srt",true));
-
-
+  //time pattern
   $pattern = '/\d{2}:\d{2}:\d{2},/';
 
-  $new_contents = '';
+  //get content of the file
+  $contents = explode("\n", file_get_contents($file,true));
 
+  //read the file and look for the time to change
+  $new_contents = '';
   foreach ($contents as $key => $line) {
 
-    //$line = "00:00:45,212 --> 00:00:47,506";
     $new_line = $line;
-    $replacements;
+
+    //check if this line is a time line
     preg_match_all($pattern, $line, $matches);
     if(count($matches)>0){
       $new_line = preg_replace_callback($pattern,
@@ -26,15 +30,21 @@
                                       },
                                       $line);
     }
+    //put every new_line in the $new_contents array
     $new_contents[$key] = $new_line;
   }
 
-
-  file_put_contents("new.srt", $new_contents);
-
-  //adjust_time("00:00:45","2")
+  // write the new file to
+  file_put_contents($new_file, $new_contents);
 
 
+  /*
+  * Adjust time function
+  * Add or substract to/from $time the amount specified in $offset.
+  * @param $time [string]: the time to be edited in the format ##:##:## (H:i:s)
+  * @param $offset [string]|[int]: the amount of seconds to be added or substracted from $time.
+  * @param $offset_time [int]: -1 to substract and 1 to add
+  */
   function adjust_time($time, $offset, $offset_type=-1){
     $date = new DateTime($time);
     $interval = new DateInterval('PT'. $offset . 'S');
@@ -46,7 +56,4 @@
     }
     return $date->format("H:i:s");
   }
-
-
-
 ?>
